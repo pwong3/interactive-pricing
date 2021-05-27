12afa25eb2a1c1a6def82a44a47f60eace41847e
pwong3 <patrickycwong@gmail.com>
2021-05-27 15:56:32 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <body>
+        <h1>Simple, traffic-based pricing</h1>
+        <p>Sign-up for our 30-day trial. No credit card required.</p>
+        Pageviews $ /month Monthly Billing Yearly Billing 25% discount
+        <ul className='list'>
+          <li>
+            <input type='checkbox' />
+            Unlimited websites
+          </li>
+          <li>
+            <input type='checkbox' />
+            100% data ownership
+          </li>
+          <li>
+            <input type='checkbox' />
+            Email reports
+          </li>
+        </ul>
+        <button className='button'>Start my trial</button>
+        <div className='attribution'>
+          Challenge by
+          <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+            Frontend Mentor
+          </a>
+          . Coded by <a href='#'>pwong3</a>.
+        </div>
+      </body>
     </div>
   );
 }
